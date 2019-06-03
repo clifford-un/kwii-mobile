dcf88108a4c9aa2ffd2a0fe17d5fc16d35423f7c
Juanito Becerra <jsbecerrab@unal.edu.co>
2019-06-02 20:36:32 -0500

    :gem: chat initial view

--- lib/screens/chatroom.dart
+++ lib/screens/chatroom.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/cupertino.dart';
+import './chatmessage.dart';
+
+
 
 class chatroom extends StatefulWidget {
   @override
@@ -8,7 +12,6 @@ class chatroom extends StatefulWidget {
 
 class _chatroomState extends State<chatroom> {
   var _vari;
-
   @override
   void initState() {
      super.initState();
@@ -20,22 +23,77 @@ class _chatroomState extends State<chatroom> {
       _vari = prefs.getString('name');
     });
   }
+  final TextEditingController _chatController = new TextEditingController();
+  final List<ChatMessage> _messages = <ChatMessage>[];
+
+  void _handleSubmit(String text) {
+    _chatController.clear();
+      ChatMessage message = new ChatMessage(
+        text: text
+    );
+      
+    setState(() {
+       _messages.insert(0, message);
+    });
+}
+
+  Widget _chatEnvironment (){
+    return IconTheme(
+      data: new IconThemeData(color: Colors.blue),
+          child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal:8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                decoration: new InputDecoration.collapsed(hintText: "Start typing ..."),
+                controller: _chatController,
+                onSubmitted: _handleSubmit,
+              ),
+            ),
+            new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                
+                onPressed: ()=> _handleSubmit(_chatController.text),
+                 
+              ),
+            )
+          ],
+        ),
+
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Appbar
-      appBar: new AppBar(
+    return new Scaffold(
+    appBar: new AppBar(
         // Title
-
+        elevation: 1,
+        backgroundColor: Color(0xFF5AA182),
         title: new Text("$_vari"),
       ),
-      // Body
-      body: new Container(
-        // Center the content
-        child: new Center(
-          // Add Text
-          child: new Text("Hello World!"),
-        ),
+    body: Column(
+        children: <Widget>[
+          new Flexible(
+            child: ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(
+            height: 1.0,
+          ),
+          new Container(decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _chatEnvironment(),)
+        ],
       ),
     );
   }
