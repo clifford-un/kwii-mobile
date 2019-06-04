3d929816e43ce3c6949cb49a91507e068cf19e4c
Juan Sebastian Peña <jspenaq@unal.edu.co>
2019-06-04 01:26:37 -0500

    :ambulance: fixing errors

--- lib/screens/chatList.dart
+++ lib/screens/chatList.dart
@@ -2,55 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './chatroom.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import './loginScreen.dart';
+import 'package:dio/dio.dart';
+
 class ChatList extends StatefulWidget {
   @override
   _ChatListState createState() => _ChatListState();
 }
 
 class _ChatListState extends State<ChatList> {
-
-
   //static List<String> dados =[
-   // 'Dia Kurosawa',
-   // 'Nadeko Sengoku',
+  // 'Dia Kurosawa',
+  // 'Nadeko Sengoku',
   //];
 
   //static List<String> imageses =[
-    //'https://static.zerochan.net/Kurosawa.Dia.full.2222565.jpg',
-    //'https://cdn.myanimelist.net/images/characters/2/264877.jpg',
-   // 'https://cdn.myanimelist.net/images/characters/2/264877.jpg',
- // ];
+  //'https://static.zerochan.net/Kurosawa.Dia.full.2222565.jpg',
+  //'https://cdn.myanimelist.net/images/characters/2/264877.jpg',
+  // 'https://cdn.myanimelist.net/images/characters/2/264877.jpg',
+  // ];
 
   //static List<String> testo =[
   //  'Quien es Nadeko?',
   //  'Holi cómo estas? UwU',
- // ];
+  // ];
 
   @override
   Widget build(BuildContext context) {
-
     _saveName(name) async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('name', name);
       print("name: $name");
     }
+
     _savePicture(picture) async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('picture', picture);
       print("name: $picture");
     }
+
     _saveMessage(message) async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('message', message);
       print("name: $message");
     }
+
     _saveChatRoomId(id) async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.setString('chat_room_id', id);
     }
-    _remover_a_la_mama_de_samael() async {
+
+    _signOut() async {
       SharedPreferences prefs = await SharedPreferences.getInstance();
       await prefs.remove("token");
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
     }
 
     final String query = r"""
@@ -65,22 +71,24 @@ class _ChatListState extends State<ChatList> {
       length: 1,
       initialIndex: 0,
       child: Scaffold(
-        
         appBar: AppBar(
           elevation: 1,
           backgroundColor: Color(0xFF5AA182),
           title: Text("KWII"),
           actions: <Widget>[
             IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(Icons.search),
             ),
             IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(Icons.message),
             ),
             IconButton(
-              onPressed: (){_remover_a_la_mama_de_samael();},
+              onPressed: () {
+                print("_signOut");
+                _signOut();
+              },
               icon: Icon(Icons.more_vert),
             )
           ],
@@ -90,112 +98,114 @@ class _ChatListState extends State<ChatList> {
             indicatorColor: Colors.white,
             tabs: <Widget>[
               //Tab(
-                //text: 'Ligações'.toUpperCase(),
+              //text: 'Ligações'.toUpperCase(),
               //),
               Tab(
                 text: "Chats".toUpperCase(),
               ),
-             // Tab(
-                //text: "Contatos".toUpperCase(),
+              // Tab(
+              //text: "Contatos".toUpperCase(),
               //)
             ],
           ),
         ),
         body: Query(
-          options: QueryOptions(document: query),
-          builder: (
-            QueryResult result, {
-            VoidCallback refetch,
-          }) {
-            if (result.loading) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (result.data == null) {
-              return Text("No Data Found !");
-            }
-            print("resultado: ${result.data['getChatroomsByUser']}");
-            return Container(
-              color: Colors.white,
-              child: ListView.builder(
-                itemCount: result.data['getChatroomsByUser'].length,
-                itemBuilder: (context, index){
+            options: QueryOptions(document: query),
+            builder: (
+              QueryResult result, {
+              VoidCallback refetch,
+            }) {
+              if (result.loading) {
+                return Center(child: CircularProgressIndicator());
+              }
+              if (result.data == null) {
+                return Text("No Data Found !");
+              }
+              print("resultado: ${result.data['getChatroomsByUser']}");
+              return Container(
+                color: Colors.white,
+                child: ListView.builder(
+                  itemCount: result.data['getChatroomsByUser'].length,
+                  itemBuilder: (context, index) {
+                    var title =
+                        result.data['getChatroomsByUser'][index]['name'];
+                    var chatRoomId =
+                        result.data['getChatroomsByUser'][index]['id'];
+                    //var imagese = imageses.elementAt(index);
+                    var testos = "Hola!";
 
-                  var title = result.data['getChatroomsByUser'][index]['name'];
-                  var chatRoomId= result.data['getChatroomsByUser'][index]['id'];
-                  //var imagese = imageses.elementAt(index);
-                  var testos = "Hola!";
-                  
-                  return  GestureDetector(
-                          onTap: (){
-                            Navigator.push(
-                            context, MaterialPageRoute(builder: (context) => chatroom()));
-                            _saveName(title);
-                           // _savePicture(imagese);
-                            _saveChatRoomId(chatRoomId);
-                            _saveMessage(testos);
-                                  },
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => chatroom()));
+                        _saveName(title);
+                        // _savePicture(imagese);
+                        _saveChatRoomId(chatRoomId);
+                        _saveMessage(testos);
+                      },
                       child: Container(
-                    width: double.maxFinite,
-                    height: 98,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: <Widget>[
-                          Row(
+                        width: double.maxFinite,
+                        height: 98,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
                             children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only( right: 8),
-                                child: Image(
-                                  width: 50,
-                                  height: 50,
-                                  image: AssetImage("assets/images/chatting.png"),
-                                ),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                              Row(
                                 children: <Widget>[
-                                  Text(title,
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8),
+                                    child: Image(
+                                      width: 50,
+                                      height: 50,
+                                      image: AssetImage(
+                                          "assets/images/chatting.png"),
                                     ),
                                   ),
-                                  Text(testos,
-                                    style: TextStyle(
-                                      fontSize: 14
-                                    ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        title,
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        testos,
+                                        style: TextStyle(fontSize: 14),
+                                      )
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  Column(
+                                    children: <Widget>[
+                                      Text(
+                                        "11:45",
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                    ],
                                   )
                                 ],
                               ),
-                              Spacer(),
-                              Column(
-                                children: <Widget>[
-                                  Text("11:45", style: TextStyle(
-                                      color: Colors.green
-                                  ),
-                                  ),
-
-                                  
-                                ],
+                              Container(
+                                margin: EdgeInsets.only(left: 50),
+                                child: Divider(
+                                  color: Colors.grey,
+                                ),
                               )
                             ],
                           ),
-                          Container(
-                            margin: EdgeInsets.only(left: 50),
-                            child: Divider(
-                              color: Colors.grey,
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                  );
-                },
-              ),
-            );
-          }),
-        ),
-      );
+                    );
+                  },
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
